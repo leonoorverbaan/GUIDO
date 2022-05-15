3dcb4f7ac67659e103071e1022594448a22007d6
Shanti Verschoor <shanti.verschoor@gmail.com>
2022-05-15 12:49:39 +0000

    Upload first haply_code test

---
+++ haply_code.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 12:15:52 2022
+
+@author: shant
+"""
+import math
+import pygame
+
+Map_dim = (600,1200)
+
+#Environment graphics
+gfx = Graphics(Map_dim, 'RobotDog.png', 'Map.png')
